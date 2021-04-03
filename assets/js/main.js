a5f627c76e70e0556bc4e00d59c40fd56da78dd7
Aliaksandra Liamnitskaya <panibarsik@gmail.com>
2021-04-03 22:33:45 +0300

    почти закончила

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').toggleClass('active');
+	});
+});
+
